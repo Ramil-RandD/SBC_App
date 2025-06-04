40eb7a18b0a79e0ed40e3bc4f77660f1b5782cee
Ramil-RandD <ramil.ziyadiev@tgtdiagnostics.com>
2025-06-04 15:25:07 +0400

    qam symbols num in frame
    calculation qam symbols number depends on qam modulation order (qam256 or qam64)

--- qam_thread.cpp
+++ qam_thread.cpp
@@ -71,6 +71,7 @@ double start_inf_data;
 creal_T backup_channel_response[13] = {{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0},{0, 0}};
 boolean_T crc_error_flag = false;
 uint8_t equal_cnt = 0;
+int qam_symbols_num = 0;
 boolean_T equalizer_adapted = false;
 boolean_T update_impalse_char = false;
 uint8_T correct_crc_cnt = 0;
@@ -377,22 +378,23 @@ void QamThread::QAM_Decoder()
                 if(equal_cnt >= 53)
                 {
                     equal_cnt = 0;
+                    qam_symbols_num = (int)(((float)qam_str.inf_byte_amount*8.0)/log2(qam_str.order));
                     if(rs_decode_flag[0] != -1 || rs_decode_flag[1] != -1)
                     {
                         if(qam_str.order == 256)
-                            qam256_modulator(frame_decoded, qam_str.inf_byte_amount, qam_symbols_decoded_ref);
+                            qam256_modulator(frame_decoded, qam_symbols_num, qam_symbols_decoded_ref);
                         else
-                            qam64_modulator(frame_decoded, (int)(((float)qam_str.inf_byte_amount*8.0)/log2(qam_str.order)), qam_symbols_decoded_ref);
+                            qam64_modulator(frame_decoded, qam_symbols_num, qam_symbols_decoded_ref);
                     }
-                    HS_EWL_LineEqualizer(qam_symbols_decoded, (int)(((float)qam_str.inf_byte_amount*8.0)/log2(qam_str.order)) + 3, qam_symbols_decoded_ref, true, equalized_data, chan_resp);
+                    HS_EWL_LineEqualizer(qam_symbols_decoded, qam_symbols_num + 3, qam_symbols_decoded_ref, true, equalized_data, chan_resp);
                     if(qam_str.order == 256)
                         pointer_to_equalized_byte = qam_256_demodulator(&equalized_data[3], qam_str.inf_byte_amount, 1, 0);
                     else
-                        pointer_to_equalized_byte = qam_64_demodulator(&equalized_data[3], (int)(((float)qam_str.inf_byte_amount*8.0)/log2(qam_str.order)) + 3, 1, 0);
+                        pointer_to_equalized_byte = qam_64_demodulator(&equalized_data[3], qam_symbols_num, 1, 0);
                     crc8_check = calc_crc8(pointer_to_equalized_byte + TxPacketRsCodesSize, TxPacketDataSize - 1);
 
-                    fir_filter(qam_symbols_decoded, qam_str.inf_byte_amount + 3, chan_resp, equalized_data);
-                    fir_filter(qam_symbols_decoded, qam_str.inf_byte_amount + 3, chan_resp_corrected, qam_symbols_equalized);
+                    fir_filter(qam_symbols_decoded, qam_symbols_num + 3, chan_resp, equalized_data);
+                    fir_filter(qam_symbols_decoded, qam_symbols_num + 3, chan_resp_corrected, qam_symbols_equalized);
 
                     update_impalse_char = LineEqualizer_qam_diagram_distance_check(&equalized_data[3], &qam_symbols_equalized[3], qam_symbols_decoded_ref);
 
