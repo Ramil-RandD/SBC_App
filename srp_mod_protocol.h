22b15ea31612aef1c843d3a213750bbe7c12c444
Ramil Ziyadiev <ramil.ziyadiev@tgtdiagnostics.com>
2026-07-07 12:58:05 +0400

    master address (0x4d) issue
    fix: The master address 0x4D is not discarded before being sent to the DLL, as it has already been discarded on the MOD2 side.

--- srp_mod_protocol.h
+++ srp_mod_protocol.h
@@ -6,7 +6,7 @@
 #define MOD_SRP_PROTOCOL_HEADER_SIZE        (3 + 0) // 1 byte addr, 2 bytes length
 #define MOD_SRP_PROTOCOL_CRC_SIZE           (2)
 #define MOD_SRP_PROTOCOL_DATA_ECC_MIN_SIZE  (0)
-#define MASTER_ADDR_SIZE                    (1)     // 'master address = 0x4d' size
+#define MASTER_ADDR_SIZE                    (0)     // 'master address = 0x4d' size
 
 #define MOD_SRP_MIN_VALID_LENGTH (MOD_SRP_PROTOCOL_HEADER_SIZE + MOD_SRP_PROTOCOL_CRC_SIZE + MOD_SRP_PROTOCOL_DATA_ECC_MIN_SIZE + MASTER_ADDR_SIZE)
 
